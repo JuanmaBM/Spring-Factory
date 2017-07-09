401af2dc04646445eb5fb4c4a6ed3de2b3e21336
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-07-09 19:34:59 +0200

    Create the base project

---
+++ src/main/java/com/jmb/springfactory/SpringFactoryApplication.java
@@ -0,0 +1,12 @@
+package com.jmb.springfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFactoryApplication.class, args);
+	}
+}
