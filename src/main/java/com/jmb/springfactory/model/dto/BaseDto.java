114c575ab4aeb7b023b82807c7b9a75b517b5844
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-08-01 00:19:12 +0200

    Added base model to entity and dto

---
+++ src/main/java/com/jmb/springfactory/model/dto/BaseDto.java
@@ -0,0 +1,14 @@
+package com.jmb.springfactory.model.dto;
+
+public class BaseDto {
+
+  private String id;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+}
