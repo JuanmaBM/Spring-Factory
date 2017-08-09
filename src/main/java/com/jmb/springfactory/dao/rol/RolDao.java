dbb0ac4dc65b547a7a2bf6e8dab44e76943eb402
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-08-09 18:01:13 +0200

    Create a repository and dao for manage roles in persistence layer

---
+++ src/main/java/com/jmb/springfactory/dao/rol/RolDao.java
@@ -0,0 +1,8 @@
+package com.jmb.springfactory.dao.rol;
+
+import com.jmb.springfactory.dao.GenericMongoDao;
+import com.jmb.springfactory.model.entity.Rol;
+
+public interface RolDao extends GenericMongoDao<Rol, String>{
+
+}
---
+++ src/main/java/com/jmb/springfactory/dao/rol/RolDaoImpl.java
@@ -0,0 +1,21 @@
+package com.jmb.springfactory.dao.rol;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
+
+import com.jmb.springfactory.dao.GenericMongoDaoImpl;
+import com.jmb.springfactory.model.entity.Rol;
+
+@Service
+public class RolDaoImpl extends GenericMongoDaoImpl<Rol, String> implements RolDao {
+  
+  @Autowired
+  private RolRepository repository;
+
+  @Override
+  public MongoRepository<Rol, String> getRepository() {
+    return repository;
+  }
+
+}
