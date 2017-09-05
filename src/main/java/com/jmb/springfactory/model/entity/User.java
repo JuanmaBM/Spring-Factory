2dbd67b8939622c4ee8098f78fe12d5e4799ad2d
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-09-05 12:01:48 +0200

    Implemented class to represente a User in controller and persistence layer

---
+++ src/main/java/com/jmb/springfactory/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.jmb.springfactory.model.entity;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document
+public class User extends BaseEntity{
+  
+  private static final String VALIDATION_EMAIL_PATTERN = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+  @Id
+  @NotNull
+  @Size(min = 9, max = 9)
+  private String nif;
+
+  @NotNull
+  private String name;
+
+  @NotNull
+  private String surname;
+
+  @NotNull
+  @Size(min = 9, max = 9)
+  private String phoneNumber;
+
+  @Pattern(regexp = VALIDATION_EMAIL_PATTERN)
+  private String email;
+}
