5093e05a4af4e6635935c6ff9a29cc5c2913ba98
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-11-05 17:41:36 +0100

    Add validation to create and update schedule and orders

--- src/main/java/com/jmb/springfactory/model/dto/ProductionOrderDTO.java
+++ src/main/java/com/jmb/springfactory/model/dto/ProductionOrderDTO.java
@@ -1,5 +1,7 @@
 package com.jmb.springfactory.model.dto;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -9,7 +11,10 @@ public class ProductionOrderDTO extends BaseDto {
 
   private static final long serialVersionUID = 1038618099762504140L;
 
+  @NotNull
   private String name;
+  @NotNull
   private String description;
+  @NotNull
   private String measurements;
 }
