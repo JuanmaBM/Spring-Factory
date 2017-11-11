5a9910855c8022d6822899d693eaf0a01ccde0be
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-11-11 12:20:56 +0100

    Creates clases to represent the work log in each the tasks registered by the users

---
+++ src/main/java/com/jmb/springfactory/model/entity/WorkLog.java
@@ -0,0 +1,21 @@
+package com.jmb.springfactory.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class WorkLog extends BaseEntity {
+
+  private Double hoursWorked;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  private Comment comment;
+}
