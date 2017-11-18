3c169a491deffcb3613cfd4f9ca743d14ebd5e6d
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2017-11-18 19:03:21 +0100

    Implements validation rules to save or update a task

--- src/main/java/com/jmb/springfactory/service/task/TaskValidatorService.java
+++ src/main/java/com/jmb/springfactory/service/task/TaskValidatorService.java
@@ -25,26 +25,42 @@ public class TaskValidatorService extends BaseService implements ValidatorServic
   private TaskMySQLService taskMySQLService;
   
   @Override
-  public void validate(Object obj) {
+  public void validateOnCreate(Object obj) {
     
     final TaskDto taskDto = (TaskDto) obj;
+    generalValidate(taskDto);
+  }
+  
+  private void generalValidate(TaskDto taskDto) {
     throwValidationExceptionIfTaskIsNull(taskDto);
-    
-    serviceLog.info("Validating if create user has not changed");
-    if (creatorHasChanged.test(taskDto)) 
-      throw new ValidationException("The task creator cant be changed");
   }
 
   final Predicate<TaskDto> creatorHasChanged = taskDto -> Optional.ofNullable(taskDto.getId())
       .flatMap(taskMySQLService::findOne)
       .map(Task::getCreator)
       .map(User::getId)
-      .map(userId -> userId.equals(taskDto.getCreator().getId()))
-      .orElse(Boolean.FALSE);
+      .map(userId -> !userId.equals(taskDto.getCreator().getId()))
+      .orElse(Boolean.TRUE);
 
   private void throwValidationExceptionIfTaskIsNull(final TaskDto taskDto) {
     if (notExist(taskDto)) 
       throw new ValidationException("The task object must not be null");
+  }
+
+  @Override
+  public void validateOnUpdate(Object obj) {
+
+    final TaskDto taskDto = (TaskDto) obj;
+    generalValidate(taskDto);
+
+    serviceLog.info("Validating if create user has not changed");
+    if (creatorHasChanged.test(taskDto)) 
+      throw new ValidationException("The task creator cant be changed");
+  }
+
+  @Override
+  public void validateOnDelete(Object object) {
+    // TODO: para eliminar comprobar que es el mismo usuario
   }
 
 }
