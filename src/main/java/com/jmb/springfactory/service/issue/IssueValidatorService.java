556080faec9587a0dccfa8fbb414774f48c2eb25
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2018-02-11 09:03:01 +0100

    Adds validations to update and deletes a issue

--- src/main/java/com/jmb/springfactory/service/issue/IssueValidatorService.java
+++ src/main/java/com/jmb/springfactory/service/issue/IssueValidatorService.java
@@ -5,13 +5,16 @@ import java.util.function.Predicate;
 
 import javax.validation.ValidationException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.jmb.springfactory.dao.issue.IssueMySQLService;
 import com.jmb.springfactory.model.dto.IssueDto;
 import com.jmb.springfactory.model.dto.PermisionDto;
 import com.jmb.springfactory.model.dto.RolDto;
 import com.jmb.springfactory.model.dto.UserDto;
+import com.jmb.springfactory.model.entity.Issue;
 import com.jmb.springfactory.model.enumeration.PermissionsEnum;
 import com.jmb.springfactory.service.BaseService;
 import com.jmb.springfactory.service.ValidatorService;
@@ -19,6 +22,9 @@ import com.jmb.springfactory.service.ValidatorService;
 @Service
 @Qualifier("issueValidatorService")
 public class IssueValidatorService extends BaseService implements ValidatorService {
+  
+  @Autowired
+  private IssueMySQLService issueMySQLService;
 
   private Predicate<IssueDto> reporterHasPermissionToManageIssues = issue -> Optional.ofNullable(issue)
       .map(IssueDto::getReporter)
@@ -31,21 +37,33 @@ public class IssueValidatorService extends BaseService implements ValidatorServi
 
   @Override
   public void validateOnCreate(Object object) {
-    final IssueDto issue = (IssueDto) object;
-    
-    if (!reporterHasPermissionToManageIssues.test(issue)) {
-      throw new ValidationException("Only user with manages issue permission can created issues");
-    }
+    // There are not any rules to create
   }
 
   @Override
   public void validateOnUpdate(Object object) {
-    // TODO Auto-generated method stub
+
+    final IssueDto issue = (IssueDto) object;
+
+    if (!reporterHasPermissionToManageIssues.test(issue)) {
+
+      final Issue storedIssue = issueMySQLService.findOne(issue.getId())
+          .orElseThrow(() -> new ValidationException("The issue not exists yet"));
+      
+      if (!storedIssue.getStatus().name().equals(issue.getStatus())) {
+        throw new ValidationException("Only users with manages issue permission can change the state of issues");
+      }
+    }
+    
   }
 
   @Override
   public void validateOnDelete(Object object) {
-    // There are not any rules to delete
+
+    final IssueDto issue = (IssueDto) object;
+    if (!reporterHasPermissionToManageIssues.test(issue)) {
+      throw new ValidationException("Only user with manages issue permission can created issues");
+    }
   }
 
 
