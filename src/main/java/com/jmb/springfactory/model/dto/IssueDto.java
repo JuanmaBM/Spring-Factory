fa8efde7aa3c6f271c2ad7b100a7bb92dac39b25
Juan Manuel Barea Martinez <juanmabareamartinez@gmail.com>
2018-02-18 08:39:21 +0100

    Adds rules to create a issue dto

--- src/main/java/com/jmb/springfactory/model/dto/IssueDto.java
+++ src/main/java/com/jmb/springfactory/model/dto/IssueDto.java
@@ -2,6 +2,8 @@ package com.jmb.springfactory.model.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -11,12 +13,15 @@ public class IssueDto extends BaseDto {
 
   private static final long serialVersionUID = 2454000008007887556L;
 
+  @NotNull
   private String name;
 
+  @NotNull
   private String description;
 
   private String status;
 
+  @NotNull
   private UserDto reporter;
 
   private UserDto reviser;
