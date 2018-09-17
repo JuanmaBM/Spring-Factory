4bd24d183884b4a0033341136065a89e7a923ac1
jmbarea <jmbarea@atsistemas.com>
2018-09-17 11:41:37 +0200

    Fixes problem with new permissions

--- src/main/java/com/jmb/springfactory/dao/permission/PermissionMySQLServiceImpl.java
+++ src/main/java/com/jmb/springfactory/dao/permission/PermissionMySQLServiceImpl.java
@@ -1,7 +1,15 @@
 package com.jmb.springfactory.dao.permission;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,20 +31,27 @@ public class PermissionMySQLServiceImpl extends GenericMySQLServiceImpl<Permissi
     @PostConstruct
     public void init() {
 
-        if (permissionRepository.count() == 0) {
+        if (permissionRepository.count() != PermissionsEnum.values().length) {
             saveDefaultPermission();
         }
     }
 
     private void saveDefaultPermission() {
 
-        for (PermissionsEnum permissionName : PermissionsEnum.values()) {
-            Permission permission = new Permission();
-            permission.setName(permissionName);
+        final List<PermissionsEnum> definedPermissions = Arrays.asList(PermissionsEnum.values());
+        final List<PermissionsEnum> storedPermissions = permissionRepository.findAll().stream().map(Permission::getName)
+                .collect(Collectors.toList());
+        final Predicate<PermissionsEnum> noContainsInStored = permission -> !storedPermissions.contains(permission);
 
-            permissionRepository.save(permission);
-        }
+        definedPermissions.stream().filter(noContainsInStored).forEach(createPermission);
     }
+
+    private Consumer<PermissionsEnum> createPermission = permission -> {
+        final Permission newPermission = new Permission();
+        newPermission.setName(permission);
+
+        permissionRepository.save(newPermission);
+    };
 
     @Override
     public Permission findPermissionByName(PermissionsEnum name) {
